2d2f40e29a8b1d7e20dd279b4f131969116ebc4d
samuel-agyabeng <agyabengsamuel88@gmail.com>
2025-09-12 10:59:47 +0000

    Add basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+numbers_sum = number1 + number2
+numbers_difference = number1 - number2
+numbers_product = number1 * number2
+print("Addition of", number1, "and", number2, "is", numbers_sum )
+print("Subtraction of", number1, "and", number2, "is", numbers_difference )
+print("Multiplication of", number1, "and", number2, "is", numbers_product )
