b22810f9cb41c44b4a72240959b45ddc726c8726
samuel-agyabeng <agyabengsamuel88@gmail.com>
2025-10-23 19:30:25 +0000

    Add perform_operation function for arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+#define function to decide the operartion to be performed
+def perform_operation (num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operation"
